1166c608690199b2eba9737ee8633331ea4877cc
lucabose80-cmd <lucabose80@gmail.com>
2026-06-19 10:53:38 +0200

    feat: Upgrade to Progressive Web App (PWA)

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'ranking-game-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './theme-starwars.css',
+  './theme-sith.css',
+  './favicon.svg',
+  './main.js'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch event (Network first, falling back to cache)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
